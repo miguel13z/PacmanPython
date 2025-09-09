45d0c3a1517c0e36c66af5f1c29cf3807cc12228
Pedro Henrique <phenrique.and13@gmail.com>
2025-09-08 21:44:06 -0300

    primeiro teste pygame

--- jogo.py
+++ jogo.py
@@ -1,3 +1,45 @@
-print('Hello world')
-print('Teste edição Pedro')
-print('Teste 2 de edição Pedro')
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((500, 500))
+
+jogador_img = pygame.image.load('img/jogador.png').convert()
+jogador_img = pygame.transform.scale(jogador_img, (20, 20))
+
+jogador_rect = jogador_img.get_rect()
+
+jogador_rect.topleft = (30, 30)
+
+running = True
+
+clock = pygame.time.Clock()
+
+while running:
+    screen.fill((0, 0, 0))
+
+    screen.blit(jogador_img, jogador_rect)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    keys = pygame.key.get_pressed()
+    
+    if keys[pygame.K_LEFT]:
+        jogador_rect.x -= 10
+    
+    if keys[pygame.K_RIGHT]:
+        jogador_rect.x += 10
+    
+    if keys[pygame.K_UP]:
+        jogador_rect.y -= 10
+    
+    if keys[pygame.K_DOWN]:
+        jogador_rect.y += 10
+
+    pygame.display.flip()
+
+    delta_time = clock.tick(120)
+
+pygame.quit()
