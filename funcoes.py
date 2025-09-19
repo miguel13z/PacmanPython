c291f5e7503b0d65f68c963c95a3fa51259af18e
Pedro Henrique <phenrique.and13@gmail.com>
2025-09-19 13:57:18 -0300

    refatorando codigo

---
+++ funcoes.py
@@ -0,0 +1,102 @@
+import pygame
+from mapa import mapa
+from math import pi
+
+def desenha_mapa(altura, largura, tela, flicker):
+    num1 = (altura - 50) // 32
+    num2 = largura // 30
+    for i in range(len(mapa)):
+        for j in range(len(mapa[i])):
+            if mapa[i][j] == 1:
+                pygame.draw.circle(tela, 'white', (j * num2 + (0.5 * num2), i * num1 + (0.5 * num1)), 2)
+            if mapa[i][j] == 2 and not flicker:
+                pygame.draw.circle(tela, 'white', (j * num2 + (0.5 * num2), i * num1 + (0.5 * num1)), 5)
+            if mapa[i][j] == 3:
+                pygame.draw.line(tela, 'blue', (j * num2 + (0.5 * num2), i * num1), (j * num2 + (0.5 * num2), i * num1 + num1), 3)
+            if mapa[i][j] == 4:
+                pygame.draw.line(tela, 'blue', (j * num2, i * num1 + (0.5 * num1)), (j * num2 + num2, i * num1 + (0.5 * num1)), 3)
+            if mapa[i][j] == 5:
+                pygame.draw.arc(tela, 'blue', [(j * num2 - (num2 * 0.4)) - 2, (i * num1 + (0.5 * num1)), num2, num1], 0, pi / 2, 3)
+            if mapa[i][j] == 6:
+                pygame.draw.arc(tela, 'blue', [(j * num2 + (num2 * 0.5)), (i * num1 + (0.5 * num1)), num2, num1], pi / 2, pi, 3)
+            if mapa[i][j] == 7:
+                pygame.draw.arc(tela, 'blue', [(j * num2 + (num2 * 0.5)), (i * num1 - (0.4 * num1)), num2, num1], pi, 3 * pi / 2, 3)
+            if mapa[i][j] == 8:
+                pygame.draw.arc(tela, 'blue', [(j * num2 - (num2 * 0.4)) - 2, (i * num1 - (0.4 * num1)), num2, num1], 3 * pi / 2, 2 * pi, 3)
+            if mapa[i][j] == 9:
+                pygame.draw.line(tela, 'white', (j * num2, i * num1 + (0.5 * num1)), (j * num2 + num2, i * num1 + (0.5 * num1)), 3)
+
+
+imagens_jogador = []
+for i in range(1, 5):
+    imagens_jogador.append(pygame.transform.scale(pygame.image.load(f'img/jogador/{i}.png'), (20, 20)))
+
+def desenha_jogador(direcao, tela, contador, jogador_x, jogador_y):
+    if direcao == 0:
+        tela.blit(imagens_jogador[contador // 5], (jogador_x, jogador_y))
+    if direcao == 1:
+        tela.blit(pygame.transform.flip(imagens_jogador[contador // 5], True, False), (jogador_x, jogador_y))
+    if direcao == 2:
+        tela.blit(pygame.transform.rotate(imagens_jogador[contador // 5], 90), (jogador_x, jogador_y))
+    if direcao == 3:
+        tela.blit(pygame.transform.rotate(imagens_jogador[contador // 5], 270), (jogador_x, jogador_y))
+
+def verifica_posicao(centro_x, centro_y, largura, altura, direcao, level):
+    pode_virar = [False, False, False, False]
+    num1 = (altura - 25) // 16
+    num2 = largura // 15
+    num3 = 7
+
+    if centro_x // 30 < 29:
+        if direcao == 0:
+            if level[centro_y // num1][(centro_x - num3) // num2] < 3:
+                pode_virar[1] = True
+        if direcao == 1:
+            if level[centro_y // num1][(centro_x + num3) // num2] < 3:
+                pode_virar[0] = True
+        if direcao == 2:
+            if level[(centro_y + num3) // num1][centro_x // num2] < 3:
+                pode_virar[3] = True
+        if direcao == 3:
+            if level[(centro_y - num3) // num1][centro_x // num2] < 3:
+                pode_virar[2] = True
+
+        if direcao == 2 or direcao == 3:
+            if 12 <= centro_x % num2 <= 18:
+                if level[(centro_y + num3) // num1][centro_x // num2] < 3:
+                    pode_virar[3] = True
+                if level[(centro_y - num3) // num1][centro_x // num2] < 3:
+                    pode_virar[2] = True
+            if 12 <= centro_y % num1 <= 18:
+                if level[centro_y // num1][(centro_x - num2) // num2] < 3:
+                    pode_virar[1] = True
+                if level[centro_y // num1][(centro_x + num2) // num2] < 3:
+                    pode_virar[0] = True
+        if direcao == 0 or direcao == 1:
+            if 12 <= centro_x % num2 <= 18:
+                if level[(centro_y + num1) // num1][centro_x // num2] < 3:
+                    pode_virar[3] = True
+                if level[(centro_y - num1) // num1][centro_x // num2] < 3:
+                    pode_virar[2] = True
+            if 12 <= centro_y % num1 <= 18:
+                if level[centro_y // num1][(centro_x - num3) // num2] < 3:
+                    pode_virar[1] = True
+                if level[centro_y // num1][(centro_x + num3) // num2] < 3:
+                    pode_virar[0] = True
+    else:
+        pode_virar[0] = True
+        pode_virar[1] = True
+
+    return pode_virar
+
+def mover_jogador(direcao, jogador_x, jogador_y, pode_virar, velocidade):
+    if direcao == 0 and pode_virar[0]:
+        jogador_x += velocidade
+    elif direcao == 1 and pode_virar[1]:
+        jogador_x -= velocidade
+    if direcao == 2 and pode_virar[2]:
+        jogador_y -= velocidade
+    elif direcao == 3 and pode_virar[3]:
+        jogador_y += velocidade
+
+    return jogador_x, jogador_y
--- main.py
+++ main.py
@@ -1,84 +1,77 @@
+import copy
 import pygame
-from gamemap import mapa
-from math import pi
-
-def desenha_mapa():
-    num1 = (altura - 50) // 32
-    num2 = largura // 30
-    for i in range(len(mapa)):
-        for j in range(len(mapa[i])):
-            if mapa[i][j] == 1:
-                pygame.draw.circle(screen, 'white', (j * num2 + (0.5 * num2), i * num1 + (0.5 * num1)), 2)
-            if mapa[i][j] == 2:
-                pygame.draw.circle(screen, 'white', (j * num2 + (0.5 * num2), i * num1 + (0.5 * num1)), 5)
-            if mapa[i][j] == 3:
-                pygame.draw.line(screen, cor_mapa, (j * num2 + (0.5 * num2), i * num1), (j * num2 + (0.5 * num2), i * num1 + num1), 3)
-            if mapa[i][j] == 4:
-                pygame.draw.line(screen, cor_mapa, (j * num2, i * num1 + (0.5 * num1)), (j * num2 + num2, i * num1 + (0.5 * num1)), 3)
-            if mapa[i][j] == 5:
-                pygame.draw.arc(screen, cor_mapa, [(j * num2 - (num2 * 0.4)) - 2, (i * num1 + (0.5 * num1)), num2, num1], 0, pi / 2, 3)
-            if mapa[i][j] == 6:
-                pygame.draw.arc(screen, cor_mapa, [(j * num2 + (num2 * 0.5)), (i * num1 + (0.5 * num1)), num2, num1], pi / 2, pi, 3)
-            if mapa[i][j] == 7:
-                pygame.draw.arc(screen, cor_mapa, [(j * num2 + (num2 * 0.5)), (i * num1 - (0.4 * num1)), num2, num1], pi, 3 * pi / 2, 3)
-            if mapa[i][j] == 8:
-                pygame.draw.arc(screen, cor_mapa, [(j * num2 - (num2 * 0.4)) - 2, (i * num1 - (0.4 * num1)), num2, num1], 3 * pi / 2, 2 * pi, 3)
-            if mapa[i][j] == 9:
-                pygame.draw.line(screen, 'white', (j * num2, i * num1 + (0.5 * num1)), (j * num2 + num2, i * num1 + (0.5 * num1)), 3)
-
-
-largura = 500
-altura = 500
-largura_pacman = 20
-altura_pacman = 20
-cor_mapa = 'blue'
+from funcoes import desenha_mapa, desenha_jogador, verifica_posicao, mover_jogador
+from mapa import mapa
 
 pygame.init()
 
-screen = pygame.display.set_mode((largura, altura))
+largura = 420
+altura = 475
+tela = pygame.display.set_mode((largura, altura))
+temporizador = pygame.time.Clock()
+fps = 60
+font = pygame.font.Font('freesansbold.ttf', 20)
+level = copy.deepcopy(mapa)
+jogador_x = 200
+jogador_y = 308
+direcao = 0
+contador = 0
+flicker = False
+pode_virar = [False, False, False, False]
+comando_direcao = 0
+velocidade = 2
 
+rodando = True
+while rodando:
+    temporizador.tick(fps)
+    tela.fill('black')
+    desenha_mapa(altura, largura, tela, flicker)
+    desenha_jogador(direcao, tela, contador, jogador_x, jogador_y)
+    centro_x = jogador_x + 10
+    centro_y = jogador_y + 10
+    pode_virar = verifica_posicao(centro_x, centro_y, largura, altura, direcao, level)
+    jogador_x, jogador_y = mover_jogador(direcao, jogador_x, jogador_y, pode_virar, velocidade)
 
-pacman = pygame.image.load('img/pacman.png').convert()
-pacman = pygame.transform.scale(pacman, (largura_pacman, altura_pacman))
-
-pacman_left = pygame.transform.flip(pacman, True, False)
-pacman_up = pygame.transform.rotate(pacman, 90)
-pacman_down = pygame.transform.rotate(pacman, -90)
-
-pacman_move = pacman
-pacman_rect = pacman_move.get_rect()
-pacman_rect.topleft = (30, 30)
-
-running = True
-clock = pygame.time.Clock()
-
-while running:
-    screen.fill((0, 0, 0))
-
-    screen.blit(pacman_move, pacman_rect)
-
-    desenha_mapa()
+    if contador < 19:
+        contador += 1
+        if contador > 3:
+            flicker = False
+    else:
+        contador = 0
+        flicker = True
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            rodando = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                comando_direcao = 0
+            if event.key == pygame.K_LEFT:
+                comando_direcao = 1
+            if event.key == pygame.K_UP:
+                comando_direcao = 2
+            if event.key == pygame.K_DOWN:
+                comando_direcao = 3
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT and comando_direcao == 0:
+               comando_direcao = direcao
+            if event.key == pygame.K_LEFT and comando_direcao == 1:
+               comando_direcao = direcao
+            if event.key == pygame.K_UP and comando_direcao == 2:
+               comando_direcao = direcao
+            if event.key == pygame.K_DOWN and comando_direcao == 3:
+               comando_direcao = direcao
 
-    keys = pygame.key.get_pressed()
-        
-    if keys[pygame.K_LEFT]:
-        pacman_move = pacman_left
-        pacman_rect.x -= 10 
-    elif keys[pygame.K_RIGHT]:
-        pacman_move = pacman
-        pacman_rect.x += 10
-    elif keys[pygame.K_UP]:
-        pacman_move = pacman_up
-        pacman_rect.y -= 10
-    elif keys[pygame.K_DOWN]:
-        pacman_move = pacman_down
-        pacman_rect.y += 10
+        for i in range(4):
+            if comando_direcao == i and pode_virar[i]:
+                direcao = i
+
+        if jogador_x > largura:
+            jogador_x = -23
+        elif jogador_x < -25:
+            jogador_x = 293
 
     pygame.display.flip()
-    clock.tick(60)
+
 
 pygame.quit()
